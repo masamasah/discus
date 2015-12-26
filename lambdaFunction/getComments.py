5ee1f224941b3a5fc5a87877f358dd9a7d440885
masayuki.hirata <masayuki.h0729@gmail.com>
2015-12-26 17:06:38 +0900

    複数同時にメッセージを取ってこれるように修正

--- lambdaFunction/getComments.py
+++ lambdaFunction/getComments.py
@@ -1,3 +1,4 @@
+# coding:utf-8
 from __future__ import print_function
 
 import json
@@ -8,11 +9,20 @@ print('Loading function')
 
 
 def lambda_handler(event, context):
-    jsonobj = {
+    jsonobj = [{
         "messageId":1,
-        "body":"Hello Lambda",
-        "author":"Masayuki.Hirata",
+        "body":u"こんにちは",
+        "author":u"将之",
+        "date": datetime.date.today().isoformat(),
+        "liked":1
+        },
+        {
+        "messageId":2,
+        "body":u"こんばんは",
+        "author":u"平田",
         "date": datetime.date.today().isoformat(),
         "liked":1
         }
+        ]
     return jsonobj
+    
--- script/index.js
+++ script/index.js
@@ -7,14 +7,9 @@ angular.module('Discus', ['ngResource'])
       'https://9l9p8myt23.execute-api.ap-northeast-1.amazonaws.com/prod/messages'
     );
 
-    function getComments(){
-      var result = [];
-      var response = Comments.get();
+    $scope.messages = Comments.query(function(data, headers){
       result.push(response);
-      return result
-    };
-
-    $scope.messages = getComments();
+    });
 
 
     $scope.reply = function(message) {
